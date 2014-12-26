d2596b3686cc10070ee63e5e55145ebb30f5732e
louspringer <github@louspringer.com>
2014-12-26 15:19:26 -0700

    Initial Commit

---
+++ hello/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  hello
+//
+//  Created by Lou Springer on 12/26/14.
+//  Copyright (c) 2014 Lou Springer. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
